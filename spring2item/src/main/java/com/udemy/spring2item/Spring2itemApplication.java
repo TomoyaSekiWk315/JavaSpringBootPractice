8433b7217eb6fbc0ee6b8463b89ef75e3af033ff
TOMOYA\stmy0 <tomoya.seki.wk@gmail.com>
2025-11-12 21:31:10 +0900

    キャッシュの作成と削除

--- spring2item/src/main/java/com/udemy/spring2item/Spring2itemApplication.java
+++ spring2item/src/main/java/com/udemy/spring2item/Spring2itemApplication.java
@@ -2,8 +2,10 @@ package com.udemy.spring2item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Spring2itemApplication {
 
 	public static void main(String[] args) {
--- spring2item/src/main/java/com/udemy/spring2item/service/ItemService.java
+++ spring2item/src/main/java/com/udemy/spring2item/service/ItemService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.udemy.spring2item.model.Item;
@@ -19,12 +22,29 @@ public class ItemService {
 			new Item("10005","ブルーベリー","フード")));
 
 	// すべてのアイテムを取得
+	
+	@Cacheable("getItems")
 	 public List<Item> getAllItems(){
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		 return allItems;
 	 }
 
 	 // 特定アイテムの取得
+	@Cacheable(value="getItem",key="#p0")
 	 public Item getItem(String itemId){
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		 for(int i=0; i < allItems.size(); i++) {
 			  if( allItems.get(i).getItemId().equals(itemId)) {
 				  return (Item)allItems.get(i);
@@ -33,10 +53,15 @@ public class ItemService {
 		 return null;
 	 }
 	 
+	 @CacheEvict(value="getItems",allEntries=true)
 	 public void addItem(Item item) {
 		 allItems.add(item);
 	 }
 	 
+	 @Caching(evict = {
+			 @CacheEvict(value="getItem",key="#p0"),
+			 @CacheEvict(value="getItems",allEntries=true)
+	 })
 	 public void updateItem(String itemId,Item item) {
 		 for(int i=0 ; i<allItems.size(); i++) {
 			 if(allItems.get(i).getItemId().equals(itemId)) {
@@ -44,7 +69,10 @@ public class ItemService {
 			 }
 		 }
 	 }
-	 
+	 @Caching(evict = {
+			 @CacheEvict(value="getItem",key="#p0"),
+			 @CacheEvict(value="getItems",allEntries=true)
+	 })
 	 public void deleteItem(String itemId) {
 		 allItems.removeIf(i -> i.getItemId().equals(itemId));
 	 }
