ff588ec2b655e8b56a3bc59e8ce3b5147fc69bed
TOMOYA\stmy0 <tomoya.seki.wk@gmail.com>
2025-11-12 21:59:32 +0900

    Junitテスト

--- spring2item/src/test/java/com/udemy/spring2item/Spring2itemApplicationTests.java
+++ spring2item/src/test/java/com/udemy/spring2item/Spring2itemApplicationTests.java
@@ -1,13 +1,32 @@
 package com.udemy.spring2item;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.udemy.spring2item.Controller.ItemController;
+import com.udemy.spring2item.service.ItemService;
 
 @SpringBootTest
 class Spring2itemApplicationTests {
+	
+	@Autowired
+	private ItemController itemController;
+	
+	@Autowired
+	private ItemService itemService;
+	
 
+	// アプリケーションがspringコンテキストを正常にロードするか
 	@Test
 	void contextLoads() {
+		// AssertJを利用した検証
+		// Asserthanの引数に検証の値を入れる
+		// 続けてメソッドにて期待値を指定。この場合はNull出ないこと
+		assertThat(itemController).isNotNull();
+		assertThat(itemService).isNotNull();
 	}
 
 }
